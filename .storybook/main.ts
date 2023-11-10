2ef3c8fb77bd9e28b0c5928f0437c35ef408b525
Luca Soares <134713702+LucaS0oares@users.noreply.github.com>
2023-11-10 14:44:10 +0000

    changes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/html-vite",
@@ -14,5 +15,9 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  staticDirs: ['../public'],
+  features: {
+    storyStoreV7: true,
+  }
 };
 export default config;
